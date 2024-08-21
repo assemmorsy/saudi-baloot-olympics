23f6e64ab187e668023e01e01f0dd37bca75dbf3
Assem Morsy <assemmorsy@outlook.com>
2024-08-22 00:15:52 +0300

    add cors

---
+++ BalootOlympicsTeamsApi/Extensions/AppBuilders/CorsConfigrations.cs
@@ -0,0 +1,23 @@
+﻿namespace BalootOlympicsTeamsApi.Extensions.AppBuilders;
+
+public static class CorsConfigurations
+{
+    public static string ConfigureCORS(this IServiceCollection services, IWebHostEnvironment environment)
+    {
+        #region Add Cors
+        string MyAllowSpecificOrigins = "_MyAllowSpecificOrigins";
+
+        services.AddCors(options =>
+        {
+            options.AddPolicy(name: MyAllowSpecificOrigins, builder =>
+            {
+                builder
+                    .AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader();
+            });
+        });
+        return MyAllowSpecificOrigins;
+        #endregion
+    }
+}
--- BalootOlympicsTeamsApi/Program.cs
+++ BalootOlympicsTeamsApi/Program.cs
@@ -5,6 +5,7 @@ builder.Configuration.ReadConfigurationFile(builder.Environment);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+string MyAllowSpecificOrigins = builder.Services.ConfigureCORS(builder.Environment);
 builder.Services.DbConfiguration(builder.Configuration, builder.Environment);
 
 builder.Services.RegisterSettings(builder.Configuration);
@@ -20,12 +21,15 @@ builder.Host.UseSerilog();
 
 var app = builder.Build();
 
+app.UseCors(MyAllowSpecificOrigins);
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 // app.UseHttpsRedirection();
+
 app.UseSerilogRequestLogging();
 app.MapCarter();
 
