0e57bb709cb598354ba6127c8108dc95bd2b0192
Assem Morsy <assemmorsy@outlook.com>
2024-08-22 22:57:25 +0300

    fix players get to confirm team

--- BalootOlympicsTeamsApi/Modules/TeamConfirmationRequests/SendOtp.cs
+++ BalootOlympicsTeamsApi/Modules/TeamConfirmationRequests/SendOtp.cs
@@ -13,7 +13,7 @@ public sealed class SendConfirmTeamOtpService(PlayerRepo _playerRepo, Confirmati
             return Result.Fail(new InvalidBodyInputError("الرقم المرجعى للاعب الاول غير موجود"));
 
         var secondPlayerRes = await _playerRepo.GetPlayerByAsync(p => p.Id == dto.SecondPlayerId, dto.SecondPlayerId);
-        if (firstPlayerRes.IsFailed)
+        if (secondPlayerRes.IsFailed)
             return Result.Fail(new InvalidBodyInputError("الرقم المرجعى للاعب الثانى غير موجود"));
 
         var firstPlayer = firstPlayerRes.Value;
